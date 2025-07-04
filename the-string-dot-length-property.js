02aa21ddce714dbd15a9c61d219dab08e031aeca
Thu Nguyen <thunguy91@gmail.com>
2025-07-04 13:02:12 -0400

    Assigned length of string to its corresponding variable.

--- the-string-dot-length-property.js
+++ the-string-dot-length-property.js
@@ -20,11 +20,14 @@ let stringTwo = "JavaScript";
 let stringThree = "";
 
 // Your task: Assign the length of each string to the corresponding variable
-let lengthOne;   // Length of stringOne
-let lengthTwo;   // Length of stringTwo
-let lengthThree; // Length of stringThree
+let lengthOne = stringOne.length;   // Length of stringOne
+let lengthTwo = stringTwo.length;   // Length of stringTwo
+let lengthThree = stringThree.length; // Length of stringThree
 
 // Your code here
+console.log("The length of " + stringOne + ": " + lengthOne);
+console.log("The length of " + stringTwo + ": " + lengthTwo);
+console.log("The length of " + stringThree + ": " + lengthThree);
 
 
 
